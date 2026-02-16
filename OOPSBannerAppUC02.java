60566ed6a86d7f2ed300653a75a4dc5fbbe1d8a7
areez-14 <areezannan2029@gmail.com>
2026-02-16 09:26:32 +0530

    asterik banner

---
+++ OOPSBannerAppUC02.java
@@ -0,0 +1,12 @@
+public class OOPSBannerAppUC02 {
+    public static void main(String[]args){
+        System.out.println("    *****         *****       ******          ******* ");
+        System.out.println("  ***   ***     ***   ***     ***    ***    ****  ****");
+        System.out.println(" ***     ***   ***     ***    ***     ***   ***       ");
+        System.out.println(" ***     ***   ***     ***    ***     ***    ***      ");
+        System.out.println(" ***     ***   ***     ***    ***    ***        ***   ");
+        System.out.println(" ***     ***   ***     ***    ******               ***");
+        System.out.println("  ***   ***     ***   ***     ***           ****  ****");
+        System.out.println("    *****         *****       ***            ******** ");
+    }
+}
