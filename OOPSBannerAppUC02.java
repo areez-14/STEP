573672fcd11a1d130266e4258c209313cf8ffac7
areez-14 <areezannan2029@gmail.com>
2026-02-16 12:41:34 +0530

    Conjugation

--- OOPSBannerAppUC02.java
+++ OOPSBannerAppUC02.java
@@ -1,12 +1,14 @@
 public class OOPSBannerAppUC02 {
     public static void main(String[]args){
-        System.out.println("    *****         *****       ******          ******* ");
-        System.out.println("  ***   ***     ***   ***     ***    ***    ****  ****");
-        System.out.println(" ***     ***   ***     ***    ***     ***   ***       ");
-        System.out.println(" ***     ***   ***     ***    ***     ***    ***      ");
-        System.out.println(" ***     ***   ***     ***    ***    ***        ***   ");
-        System.out.println(" ***     ***   ***     ***    ******               ***");
-        System.out.println("  ***   ***     ***   ***     ***           ****  ****");
-        System.out.println("    *****         *****       ***            ******** ");
+    System.out.println("      ******      "   +   "       ******      "   +   "  ********      "   +   "     ******      " );
+    System.out.println("    **      **    "   +   "     **      **    "   +   "  **      **    "   +   "   **            " );
+    System.out.println("  **          **  "   +   "   **          **  "   +   "  **        **  "   +   "  **             " );
+    System.out.println("  **          **  "   +   "   **          **  "   +   "  **      **    "   +   "    **           " );
+    System.out.println("  **          **  "   +   "   **          **  "   +   "  ** ******     "   +   "      ****       " );
+    System.out.println("  **          **  "   +   "   **          **  "   +   "  **            "   +   "           **    " );
+    System.out.println("  **          **  "   +   "   **          **  "   +   "  **            "   +   "            **   " );
+    System.out.println("    **      **    "   +   "     **      **    "   +   "  **            "   +   "          **     " );
+    System.out.println("      ******      "   +   "       ******      "   +   "  **            "   +   "    ******       " );
+    
     }
 }
